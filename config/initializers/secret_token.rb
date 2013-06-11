7fb712af2bdb2d941852e6ecc460e705fde1be85
appertree <me@lowellfortune.com>
2013-06-11 11:38:27 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pigine::Application.config.secret_token = 'f8c0ca0edc9c7926649750065c3d57d65520072e2023f38738575b0afa276414a9b0ab59e69c451401dc95d59528174335d1e16f5d04ea53c99873fe0d3e144e'
